a314957f314790b50e957c5f6b8c21e561bc1210
bagonyidori <dori.bagonyi@gmail.com>
2026-03-23 13:53:00 +0100

    Movie table structure

--- database/migrations/2026_03_23_122831_create_movies_table.php
+++ database/migrations/2026_03_23_122831_create_movies_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,14 @@ return new class extends Migration
     {
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
+            $table->string("title")->unique();
+            $table->string("type");
+            $table->string("genre");
+            $table->string("plot");
+            $table->year("releaseYear");
+            $table->string("poster");
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
