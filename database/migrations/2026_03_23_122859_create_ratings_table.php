21b7349d100f2acf8fe8d3efd4b6bd8801389548
bagonyidori <dori.bagonyi@gmail.com>
2026-03-23 14:04:20 +0100

    Ratings table structure

--- database/migrations/2026_03_23_122859_create_ratings_table.php
+++ database/migrations/2026_03_23_122859_create_ratings_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,11 @@ return new class extends Migration
     {
         Schema::create('ratings', function (Blueprint $table) {
             $table->id();
+            $table->id("movieId");
+            $table->id("userId");
+            $table->string("comment");
+            $table->integer("stars");
+            $table->softDeletes();
             $table->timestamps();
         });
     }
