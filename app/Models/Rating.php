615c6a9419454494068ddad954960e5a1a2b273a
bagonyidori <dori.bagonyi@gmail.com>
2026-03-24 12:05:20 +0100

    use SoftDeletes added to Rating.php

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -4,17 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Rating extends Model
 {
     /** @use HasFactory<\Database\Factories\RatingFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         "comment",
         "stars"
     ];
 
+    protected $dates = ['deleted_at'];
     public function users()
     {
         return $this->belongsTo(User::class);
