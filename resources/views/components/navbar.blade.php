16bbb604a84b140d000d4e28c601a90a495c894b
barna_márk <mbarna007@gmail.com>
2026-03-24 13:13:38 +0100

    Added links to navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,16 +1,16 @@
 <nav class="navbar">
-    <a class="nav_brand" href="">
+    <a class="nav_brand" href="{{ route('home') }}">
         <span class="nav_logo">C</span>
         <span>Criticly</span>
     </a>
 
-    <form class="nav_search" action="" method="GET">
+    <form class="nav_search" action="{{ route('movies.index') }}" method="GET">
         <input type="search" name="q" placeholder="Keresés filmekre, sorozatokra..." autocomplete="off">
         <button type="submit">Keresés</button>
     </form>
 
     <div class="nav_links">
-        <a href="">Főoldal</a>
-        <a href="">Filmek</a>     
+        <a href="{{ route('home') }}">Főoldal</a>
+        <a href="{{ route('movies.index') }}">Filmek</a>       
     </div>
 </nav>
