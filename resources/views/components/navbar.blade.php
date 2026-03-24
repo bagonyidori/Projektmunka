160e5c9580422720660c81ed76191c9bee23c4e1
barna_márk <mbarna007@gmail.com>
2026-03-24 12:58:36 +0100

    Added navbar content

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar">
+    <a class="nav_brand" href="">
+        <span class="nav_logo">C</span>
+        <span>Criticly</span>
+    </a>
+
+    <form class="nav_search" action="" method="GET">
+        <input type="search" name="q" placeholder="Keresés filmekre, sorozatokra..." autocomplete="off">
+        <button type="submit">Keresés</button>
+    </form>
+
+    <div class="nav_links">
+        <a href="">Főoldal</a>
+        <a href="">Filmek</a>
+        
+    </div>
+</nav>
