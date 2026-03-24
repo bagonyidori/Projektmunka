5c7df303eebd33bc9b0a103c8ec1c7a12aaa9db1
bagonyidori <dori.bagonyi@gmail.com>
2026-03-24 16:59:58 +0100

    fixed getMovies function

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -14,23 +14,28 @@ class MovieController extends Controller
         $pages = 25;
         for ($page = 1; $page <= $pages; $page++) {
             $resp = Http::get('https://api.themoviedb.org/3/movie/popular', ['api_key' => '69347c0868fbf37f48034f15e356362b', 'page' => $page]);
-        }
 
-        $genres = ['Comedy', 'Action', 'Horror', 'Rom-Com', 'Thriller', 'Sci-Fi', 'Drama', 'Romance', 'Fantasy'];
-        $data = $resp->json();
-        foreach ($data['results'] as $movieData) {
-            Movie::updateOrCreate(
-                [
-                    'tmdb_id' => $movieData['id']
-                ],
-                [
-                    'title' => $movieData['original_title'],
-                    'plot' => $movieData['overview'],
-                    'genre' => fake()->randomElement($genres),
-                    'poster' => $movieData['poster_path'],
-                    'releaseDate' => $movieData['release_date']
-                ]
-            );
+            $genres = ['Comedy', 'Action', 'Horror', 'Rom-Com', 'Thriller', 'Sci-Fi', 'Drama', 'Romance', 'Fantasy'];
+            $data = $resp->json();
+            foreach ($data['results'] as $movieData) {
+
+                if (!isset($movieData['id'])) {
+                    continue;
+                }
+
+                Movie::updateOrCreate(
+                    [
+                        'tmdb_id' => $movieData['id']
+                    ],
+                    [
+                        'title' => $movieData['original_title'],
+                        'plot' => $movieData['overview'],
+                        'genre' => fake()->randomElement($genres),
+                        'poster' => $movieData['poster_path'],
+                        'releaseDate' => $movieData['release_date']
+                    ]
+                );
+            }
         }
     }
 
@@ -95,7 +100,7 @@ class MovieController extends Controller
         //
     }
 
-   
+
     public function home()
     {
         $featured = Movie::latest()->take(6)->get();
