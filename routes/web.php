1e33e69a5cc95c9dc203a7288113c5e1da87c354
bagonyidori <dori.bagonyi@gmail.com>
2026-03-24 10:37:06 +0100

    getMovies Route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get('/getMovies', [MovieController::class, 'getMovies']);
