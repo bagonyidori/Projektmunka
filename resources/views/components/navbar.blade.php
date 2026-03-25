2e7f726afe449a6cd4ad4919d48d6965f527d66a
barna_márk <mbarna007@gmail.com>
2026-03-25 09:38:34 +0100

    Added about, login and register buttons

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,12 +5,16 @@
     </a>
 
     <form class="nav_search" action="{{ route('movies.index') }}" method="GET">
-        <input type="search" name="q" placeholder="Keresés filmekre, sorozatokra..." autocomplete="off">
-        <button type="submit">Keresés</button>
+        <input type="search" name="q" placeholder="Keresés..." autocomplete="off">
     </form>
 
     <div class="nav_links">
         <a href="{{ route('home') }}">Főoldal</a>
         <a href="{{ route('movies.index') }}">Filmek</a>       
+        <a href="{{ route('about') }}">Rólunk</a> </div>
+
+    <div class="nav_auth">
+        <a href="{{ route('login') }}" class="btn btn--ghost">Belépés</a>
+        <a href="{{ route('register') }}" class="btn btn--primary">Regisztráció</a>
     </div>
 </nav>
