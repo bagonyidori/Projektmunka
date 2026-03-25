e5cf15ed90ed972289b5385a30418c893f5c184e
barna_márk <mbarna007@gmail.com>
2026-03-25 10:09:20 +0100

    Added static routes for frontend development purposes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 Route::get('/', function () {
@@ -14,3 +15,15 @@ Route::get('/getMovies', [MovieController::class, 'getMovies']);
 Route::get('/', [MovieController::class, 'home'])->name('home');
 Route::get('/movies', [MovieController::class, 'index'])->name('movies.index');
 Route::get('/movies/{id}', [MovieController::class, 'show'])->name('movies.show');
+
+Route::get('/rolunk', function () {
+    return view('pages.about');
+})->name('about');
+
+Route::get('/bejelentkezes', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::get('/regisztracio', function () {
+    return view('auth.register');
+})->name('register');
