dfdc6bff2f0a75e55514f5bd499272c212741fee
barna_márk <mbarna007@gmail.com>
2026-03-25 22:05:23 +0100

    Added favorite and share buttons

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -28,6 +28,17 @@
                 <h3>Történet</h3>
                 <p>{{ $movie->plot }}</p>
             </div>
+            <div class="movie_actions">
+                <button id="favBtn" class="btn btn--primary {{ $movie->is_favorite ? 'is_active' : '' }}" data-id="{{ $movie->id }}">
+                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                Kedvencekhez
+                </button>
+    
+                <button id="shareBtn" class="btn btn--ghost">
+                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path><polyline points="16 6 12 2 8 6"></polyline><line x1="12" y1="2" x2="12" y2="15"></line></svg>
+                Megosztás
+                </button>
+            </div>
         </div>
     </div>
 </div>
