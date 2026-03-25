7350f7b406334db037049b644aa2037b22293c63
barna_márk <mbarna007@gmail.com>
2026-03-25 22:21:57 +0100

    Fixed and updated rating placeholder

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -22,6 +22,10 @@
             <div class="movie_badges">
                 <span class="badge">{{ date('Y-m-d', strtotime($movie->releaseDate)) }}</span>
                 <span class="badge">{{ $movie->genre }}</span>
+                <span class="badge badge--rating">
+                    <svg viewBox="0 0 24 24" fill="currentColor" style="width:14px; height:14px; margin-right:4px;"><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path></svg>
+                    {{ $movie->rating }}
+                </span>
             </div>
             
             <div class="movie_plot">
