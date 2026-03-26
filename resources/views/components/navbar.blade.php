d70e0919bb0edfca926e8f755c042eef79b8a46b
bagonyidori <dori.bagonyi@gmail.com>
2026-03-26 18:35:06 +0100

    updating href for login and register

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -6,7 +6,7 @@
 
     <form class="nav_search" action="{{ route('movies.index') }}" method="GET">
         <input type="search" name="search" placeholder="Keresés..." value="{{ request('search') }}" autocomplete="off">
-        
+
         @if(request('genre'))
             <input type="hidden" name="genre" value="{{ request('genre') }}">
         @endif
@@ -14,12 +14,12 @@
 
     <div class="nav_links">
         <a href="{{ route('home') }}">Főoldal</a>
-        <a href="{{ route('movies.index') }}">Filmek</a>       
-        <a href="{{ route('about') }}">Rólunk</a> 
+        <a href="{{ route('movies.index') }}">Filmek</a>
+        <a href="{{ route('about') }}">Rólunk</a>
     </div>
 
     <div class="nav_auth">
-        <a href="{{ route('login') }}" class="btn btn--ghost">Belépés</a>
-        <a href="{{ route('register') }}" class="btn btn--primary">Regisztráció</a>
+        <a href="{{ route('show.login') }}" class="btn btn--ghost">Belépés</a>
+        <a href="{{ route('show.register') }}" class="btn btn--primary">Regisztráció</a>
     </div>
 </nav>
