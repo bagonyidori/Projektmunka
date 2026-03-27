9485e81515994f37017437a254a2fdc94dee3950
alexbarothi <alexbarothi@gmail.com>
2026-03-27 17:05:12 +0100

    Setup laravel API request connection

--- CritiqlyAdmin/MainPage.xaml.cs
+++ CritiqlyAdmin/MainPage.xaml.cs
@@ -14,5 +14,31 @@ namespace CritiqlyAdmin
         {
             InitializeComponent();
         }
+
+        //var movies = await GetAsync<Movie>("http://10.0.2.2:8000/api/movies");
+        //var users = await GetAsync<User>("http://10.0.2.2:8000/api/users");
+        //var reviews = await GetAsync<Review>("http://10.0.2.2:8000/api/reviews");
+
+        public async Task<List<T>> GetAsync<T>(string url)
+        {
+            var client = new HttpClient();
+
+            var response = await client.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+                throw new Exception("API error");
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            var result = JsonSerializer.Deserialize<List<T>>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            if (result == null)
+                throw new Exception("Deserialization failed");
+
+            return result;
+        }
     }
 }
