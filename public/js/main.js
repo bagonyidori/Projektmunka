33005cd4698160c0598cf40ce6a85bebe0e8ade4
alexbarothi <alexbarothi@gmail.com>
2026-03-27 19:47:03 +0100

    Changed favourite button mechanics

--- public/js/main.js
+++ public/js/main.js
@@ -25,6 +25,24 @@ document.addEventListener('DOMContentLoaded', () => {
         favBtn.addEventListener('click', () => {
             favBtn.classList.toggle('is_active');
             showNotification(favBtn.classList.contains('is_active') ? 'Hozzáadva!' : 'Eltávolítva.');
+            if (favBtn.classList.contains('is_active')) {
+                favBtn.innerHTML = `
+                    <svg class="favBtn-heart" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M12 21.23L4.22 13.45a5.5 5.5 0 0 1 7.78-7.78L12 5.67" />
+                        <path d="M12 5.67l1.06-1.06a5.5 5.5 0 0 1 7.78 7.78L12 21.23" />
+                        <path d="M12 5.67l-2 4 3 2-2 4" />
+                    </svg>
+                    Eltávolítás
+                `;
+            }
+            else {
+                favBtn.innerHTML = `
+                    <svg class="favBtn-heart" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                        <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+                    </svg>
+                    Kedvencekhez
+                `;
+            }
         });
     }
 
--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -33,8 +33,8 @@
                 <p>{{ $movie->plot }}</p>
             </div>
             <div class="movie_actions">
-                <button id="favBtn" class="btn btn--primary {{ $movie->is_favorite ? 'is_active' : '' }}" data-id="{{ $movie->id }}">
-                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                <button id="favBtn" class="btn favBtn btn--primary {{ $movie->is_favorite ? 'is_active' : '' }}" data-id="{{ $movie->id }}">
+                <svg class="favBtn-heart" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
                 Kedvencekhez
                 </button>
     
