175dec4449ffac0861a8a77b0ef1d9600d3cf702
alexbarothi <alexbarothi@gmail.com>
2026-03-28 14:35:36 +0100

    Movie genre request added

--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -14,17 +14,33 @@ class MovieSeeder extends Seeder
      */
     public function run(): void
     {
+        $genreResp = Http::get('https://api.themoviedb.org/3/genre/movie/list', ['api_key' => env('TMBD_API_KEY')]);
+        $genreData = $genreResp->json();
+
         $pages = 25;
         for ($page = 1; $page <= $pages; $page++) {
             $resp = Http::get('https://api.themoviedb.org/3/movie/popular', ['api_key' => env('TMBD_API_KEY'), 'page' => $page]);
+            
 
-            $genres = ['Comedy', 'Action', 'Horror', 'Rom-Com', 'Thriller', 'Sci-Fi', 'Drama', 'Romance', 'Fantasy'];
+            //$genres = ['Comedy', 'Action', 'Horror', 'Rom-Com', 'Thriller', 'Sci-Fi', 'Drama', 'Romance', 'Fantasy'];
             $data = $resp->json();
+
             foreach ($data['results'] as $movieData) {
 
                 if (!isset($movieData['id']) || empty($movieData['release_date'])) {
                     continue;
                 }
+
+                $genreIds = $movieData['genre_ids'];
+                $names = [];
+                foreach ($genreData['genres'] as $genre)
+                {
+                    if(in_array($genre['id'], $genreIds))
+                    {
+                        $names[] = $genre['name'];
+                    }
+                }
+                $genreStr = implode(", ", $names);
 
                 Movie::updateOrCreate(
                     [
@@ -32,8 +48,8 @@ class MovieSeeder extends Seeder
                     ],
                     [
                         'title' => $movieData['title'],
+                        'genre' => $genreStr,
                         'plot' => $movieData['overview'],
-                        'genre' => fake()->randomElement($genres),
                         'poster' => $movieData['poster_path'],
                         'releaseDate' => $movieData['release_date']
                     ]
