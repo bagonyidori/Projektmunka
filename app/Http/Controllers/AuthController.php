1040bffccf81b05fcb148b147214c3ee9bbfd542
bagonyidori <dori.bagonyi@gmail.com>
2026-03-29 13:47:27 +0200

    Logout function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -38,4 +38,15 @@ class AuthController extends Controller
     {
 
     }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('login');
+
+    }
 }
