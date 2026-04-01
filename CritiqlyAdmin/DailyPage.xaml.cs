d252b7cdc877db78ecf425e11ce15821d97b1e5e
alexbarothi <alexbarothi@gmail.com>
2026-04-02 01:16:40 +0200

    DailyPage complete (for now)

--- CritiqlyAdmin/DailyPage.xaml.cs
+++ CritiqlyAdmin/DailyPage.xaml.cs
@@ -25,52 +25,44 @@ public partial class DailyPage : ContentPage
     {
         base.OnAppearing();
 
+        EntryQuery.Text = "";
         StatusLabel.Text = "Utolsó frissítés: " + AppData.DailyLastUpdate?.ToString("yyyy.MM.dd HH:mm") ?? "N/I";
 
         SelectedIds.Clear();
         QueryMovies.Clear();
     }
 
-    public void SearchQuery(Object sender, EventArgs e)
+    public async void SearchQuery(Object sender, EventArgs e)
     {
+        searchQueryBtn.BackgroundColor = Colors.Orange;
         QueryMovies.Clear();
         foreach (var movie in AppData.Movies)
         {
-            if (movie.title.Contains(EntryQuery.Text))
+            if (movie.title.ToLower().Contains(EntryQuery.Text.ToLower()))
             {
                 QueryMovies.Add(movie);
             }
         }
+        await Task.Delay(1000);
+        searchQueryBtn.BackgroundColor = Color.FromRgb(212, 255, 62);
     }
 
-    public void RollRandom(Object sender, EventArgs e)
+    public async void RollRandom(Object sender, EventArgs e)
     {
+        rollRandomBtn.BackgroundColor = Colors.Orange;
         int count = SelectedIds.Count;
         int currentNum = 0;
         if(count != 4)
         {
             currentNum = 4 - count;
-        }
 
-        Random rnd = new Random();
-        for (int i = 0; i < currentNum; i++)
-        {
-            SelectedIds.Add(rnd.Next(1, (AppData.Movies.Count + 1)));
-        }
-    }
-
-    public async void AddToDailyList(Object sender, EventArgs e)
-    {
-        var Button = sender as Button;
-        var id = Button?.CommandParameter;
-
-        Button.BackgroundColor = Colors.Orange;
-        await Task.Delay(3000);
-        Button.BackgroundColor = Color.FromRgb(212, 255, 62);
-        
-        if (SelectedIds.Count != 4)
-        {
-            SelectedIds.Add((Int32)id);
+            Random rnd = new Random();
+            for (int i = 0; i < currentNum; i++)
+            {
+                SelectedIds.Add(rnd.Next(1, (AppData.Movies.Count + 1)));
+                await Task.Delay(500);
+                rollRandomBtn.BackgroundColor = Color.FromRgb(212, 255, 62);
+            }
         }
         else
         {
@@ -78,36 +70,85 @@ public partial class DailyPage : ContentPage
         }
     }
 
-    public async void Save(Object sender, EventArgs e)
+    public async void AddToDailyList(Object sender, EventArgs e)
     {
-        var client = new HttpClient();
-
-        var dailyData = new
+        var Button = sender as Button;
+        var id = Button?.CommandParameter;
+        
+        if (SelectedIds.Count != 4 && !SelectedIds.Contains((Int32)id))
         {
-            movies = SelectedIds.ToArray(),
-        };
-        var dailyJson = JsonSerializer.Serialize(dailyData);
-        var httpDailyData = new StringContent(dailyJson, Encoding.UTF8, "application/json");
-
-        var responseDaily = await client.PostAsync("http://localhost:8000/api/daily-movies", httpDailyData);
-
-        var dateData = new
+            Button.BackgroundColor = Colors.Orange;
+            SelectedIds.Add((Int32)id);
+            await Task.Delay(500);
+            Button.BackgroundColor = Color.FromRgb(212, 255, 62);
+        }
+        else if (SelectedIds.Contains((Int32)id))
         {
-            daily = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:sszzz"),
-            trending = AppData.TrendingLastUpdate.Value.ToString("yyyy-MM-ddTHH:mm:sszzz")
-        };
-        var dateJson = JsonSerializer.Serialize(dateData);
-        var httpDateDate = new StringContent(dateJson, Encoding.UTF8, "application/json");
-        //await DisplayAlertAsync("json", json, "OK");
-        var responseDate = await client.PostAsync("http://localhost:8000/api/admin/update", httpDateDate);
-
-        if (responseDate.IsSuccessStatusCode && responseDaily.IsSuccessStatusCode)
-        {
-            await Shell.Current.GoToAsync($"//MainPage");
+            Button.BackgroundColor = Colors.Red;
+            SelectedIds.Remove((Int32)id);
+            await Task.Delay(500);
+            Button.BackgroundColor = Color.FromRgb(212, 255, 62);
+            checkSelected(this, EventArgs.Empty);
         }
         else
         {
-            await DisplayAlertAsync("Hiba", "A mentés nem sikerült! \n Próbáld újra!", "OK");
+            await DisplayAlertAsync("INFO", "Elérted a maximálisan hozzáadható filmek számát!", "OK");
+        }
+    }
+
+    public async void checkSelected(Object sender, EventArgs e)
+    {
+        QueryMovies.Clear();
+        checkSelectedBtn.BackgroundColor = Colors.Orange;
+
+        foreach(var movie in AppData.Movies)
+        {
+            if (SelectedIds.Contains(movie.id))
+            {
+                QueryMovies.Add(movie);
+            }
+        }
+        await Task.Delay(500);
+        checkSelectedBtn.BackgroundColor = Color.FromRgb(212, 255, 62);
+    }
+
+    public async void Save(Object sender, EventArgs e)
+    {
+        if (SelectedIds.Count > 2)
+        {
+            var client = new HttpClient();
+
+            var dailyData = new
+            {
+                movies = SelectedIds.ToArray(),
+            };
+            var dailyJson = JsonSerializer.Serialize(dailyData);
+            var httpDailyData = new StringContent(dailyJson, Encoding.UTF8, "application/json");
+
+            var responseDaily = await client.PostAsync("http://localhost:8000/api/daily-movies", httpDailyData);
+
+            var dateData = new
+            {
+                daily = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:sszzz"),
+                trending = AppData.TrendingLastUpdate.Value.ToString("yyyy-MM-ddTHH:mm:sszzz")
+            };
+            var dateJson = JsonSerializer.Serialize(dateData);
+            var httpDateDate = new StringContent(dateJson, Encoding.UTF8, "application/json");
+            //await DisplayAlertAsync("json", json, "OK");
+            var responseDate = await client.PostAsync("http://localhost:8000/api/admin/update", httpDateDate);
+
+            if (responseDate.IsSuccessStatusCode && responseDaily.IsSuccessStatusCode)
+            {
+                await Shell.Current.GoToAsync($"//MainPage");
+            }
+            else
+            {
+                await DisplayAlertAsync("Hiba", "A mentés nem sikerült! \n Próbáld újra!", "OK");
+            }
+        }
+        else
+        {
+            await DisplayAlertAsync("HIBA", "A feltölteni kívánt filmek száma nem elegendő!", "OK");
         }
     }
 }
