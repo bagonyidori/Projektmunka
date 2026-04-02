f317bd6216c5aab20eb74b0cba91b52424154951
barna_márk <mbarna007@gmail.com>
2026-04-02 11:00:52 +0200

    Removed not yet made sections from profile

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -14,28 +14,14 @@
         <section class="profile_section reveal">
             <h2>Kedvenc filmjeid</h2>
             <div class="grid">
-                @forelse($favoriteMovies as $movie)
-                    @include('components.movie-card', ['movie' => $movie])
-                @empty
-                    <p>Nincsenek kedvenc filmjeid. Fedezz fel újakat a <a href="{{ route('movies.index') }}">filmek között</a>!</p>
-                @endforelse
+                
             </div>
         </section>
 
         <section class="profile_section reveal">
             <h2>Saját értékeléseid</h2>
             <div class="my_review_list">
-                @forelse($myReviews as $review)
-                    <div class="my_review_card">
-                        <strong>{{ $review->movie->title }}</strong>
-                        <p>{{ $review->content }}</p>
-                        <span class="rating">⭐ {{ $review->rating }}/10</span>
-                    </div>
-                    <small>{{ $review->created_at->format('Y. M. d.') }}</small>
-                    <p>{{ $review->content }}</p>
-                @empty
-                    <p>Nem írtál még értékelést. Oszd meg véleményed a filmekről!</p>
-                @endforelse
+                
             </div>
         </section>
     </div>
