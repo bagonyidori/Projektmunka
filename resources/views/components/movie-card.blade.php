f14a542c94581b564a8a4b14b2ba77fa9b88410e
barna_márk <mbarna007@gmail.com>
2026-04-04 20:25:24 +0200

    Added placeholder for movie card ratings

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -3,7 +3,7 @@
         <img src="https://image.tmdb.org/t/p/original{{ $movie->poster }}" alt="{{ $movie->title }}">
         <div class="card_rating">
              <svg viewBox="0 0 24 24" fill="currentColor"><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path></svg>
-             <span>{{ $movie->rating }}</span>
+             <span>{{ number_format($movie->average_rating, 1) }}</span>
         </div>
     </div>
     
