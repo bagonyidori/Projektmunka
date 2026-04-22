b8a90af20d8a573f84a67a670575c486cc6e8837
alexbarothi <alexbarothi@gmail.com>
2026-04-22 15:05:42 +0200

    Added URL table to Movies in migration

--- database/migrations/2026_03_23_122831_create_movies_table.php
+++ database/migrations/2026_03_23_122831_create_movies_table.php
@@ -18,6 +18,7 @@ return new class extends Migration {
             $table->text("plot");
             $table->date("releaseDate");
             $table->string("poster")->nullable();
+            $table->string("trailerUrl");
             $table->timestamps();
             $table->softDeletes();
         });
