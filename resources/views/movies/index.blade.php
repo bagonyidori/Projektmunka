56ff0af7082ded546839ced2ba31c54eaa9baf11
barna_márk <mbarna007@gmail.com>
2026-04-24 16:23:24 +0200

    Fixed movie filters

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -8,17 +8,16 @@
         <h1>Filmek</h1>
         <p>Szűrj, keress és fedezz fel új kedvenceket.</p>
     </div>
-
     <div class="filters">
         <a href="{{ route('movies.index') }}" class="chip {{ !request('genre') ? 'is_active' : '' }}">Összes</a>  
-        <a href="{{ route('movies.index', ['genre' => 'Action']) }}" class="chip {{ request('genre') == 'Action' ? 'is_active' : '' }}">Akció</a>
-        <a href="{{ route('movies.index', ['genre' => 'Comedy']) }}" class="chip {{ request('genre') == 'Comedy' ? 'is_active' : '' }}">Vígjáték</a>
-        <a href="{{ route('movies.index', ['genre' => 'Drama']) }}" class="chip {{ request('genre') == 'Drama' ? 'is_active' : '' }}">Dráma</a>
+        <a href="{{ route('movies.index', ['genre' => 'Akció']) }}" class="chip {{ request('genre') == 'Akció' ? 'is_active' : '' }}">Akció</a>
+        <a href="{{ route('movies.index', ['genre' => 'Vígjáték']) }}" class="chip {{ request('genre') == 'Vígjáték' ? 'is_active' : '' }}">Vígjáték</a>
+        <a href="{{ route('movies.index', ['genre' => 'Dráma']) }}" class="chip {{ request('genre') == 'Dráma' ? 'is_active' : '' }}">Dráma</a>
         <a href="{{ route('movies.index', ['genre' => 'Sci-Fi']) }}" class="chip {{ request('genre') == 'Sci-Fi' ? 'is_active' : '' }}">Sci-Fi</a>
         <a href="{{ route('movies.index', ['genre' => 'Horror']) }}" class="chip {{ request('genre') == 'Horror' ? 'is_active' : '' }}">Horror</a>
-        <a href="{{ route('movies.index', ['genre' => 'Romance']) }}" class="chip {{ request('genre') == 'Romance' ? 'is_active' : '' }}">Romantikus</a>
+        <a href="{{ route('movies.index', ['genre' => 'Romantikus']) }}" class="chip {{ request('genre') == 'Romantikus' ? 'is_active' : '' }}">Romantikus</a>
         <a href="{{ route('movies.index', ['genre' => 'Fantasy']) }}" class="chip {{ request('genre') == 'Fantasy' ? 'is_active' : '' }}">Fantasy</a>
-    </div>
+</div>
 </section>
 
 <section class="grid" id="catalogGrid">
