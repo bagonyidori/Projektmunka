f1389cb286d38dcdf2fd4ed3ad0d9ba1f547b1bc
alexbarothi <alexbarothi@gmail.com>
2026-04-26 10:23:25 +0200

    Added index function to VoteController and created skeleton for update function

--- app/Http/Controllers/Api/VoteController.php
+++ app/Http/Controllers/Api/VoteController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Models\StreamingVote;
 
 class VoteController extends Controller
 {
-    //
+    public function index()
+    {
+        return StreamingVote::all();
+    }
+
+    public function update(Request $request, StreamingVote $streamingVote)
+    {
+        //
+    }
 }
