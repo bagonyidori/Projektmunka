a0840e0d772fe5b26d3fd19259dbd63fb5a14768
bagonyidori <dori.bagonyi@gmail.com>
2026-04-27 13:12:49 +0200

    user login test passed

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use function Laravel\Prompts\password;
+
 
 class AuthTest extends TestCase
 {
@@ -20,7 +23,7 @@ class AuthTest extends TestCase
      }*/
 
 
-    public function user_can_register()
+    public function test_user_can_register()
     {
         $this->post('/register', [
             'name' => 'Test User',
@@ -34,5 +37,19 @@ class AuthTest extends TestCase
             'email' => 'test@user.com',
             'username' => 'testuser'
         ]);
+    }
+
+    public function test_user_can_login()
+    {
+        $user = User::factory()->create([
+            'password' => 'Password1234'
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'Password1234'
+        ]);
+
+        $this->assertAuthenticated();
     }
 }
